1d07ab0b379c1b1c45ac8896410b84aa9139798f
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-10-24 19:56:41 +0530

    add suggestionbar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,16 +1,51 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { SEARCH_SUGGESTION_API } from "../utils/config";
+import store from "../utils/store";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
-    const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState("")
+    const [suggestions, setSuggestions] = useState([])
+    const [showSuggestion, setShowsuggestion] = useState(false)
 
+    const searchCache = useSelector(store => store.search);
+        
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (searchCache[searchQuery]) {
+                setSuggestions(searchCache[searchQuery])
+            } else {
+                getSearchSuggestion()
+            }
+        }, 200);
+        return () => {
+            console.log("clean")
+            clearTimeout(timer);
+        }
+    }, [searchQuery])
+
+    const getSearchSuggestion = async () => {
+        console.log(searchQuery)
+        const data = await fetch(SEARCH_SUGGESTION_API + searchQuery)
+        const json = await data.json();
+        console.log("suggestion", json[1])
+        setSuggestions(json[1])
+
+        // update cache
+        dispatch(cacheResults({
+            [searchQuery]: json[1]
+        }))
+    }
+
+    const dispatch = useDispatch();
     const toggleMenuHandler = () => {
         dispatch(toggleMenu())
     }
 
     return (
-        <div className="grid grid-flow-col  px-2 m-2 h-14 shadow-lg">
+        <div className="grid grid-flow-col  px-2 m-2 h-14 shadow-lg sticky top-0 bg-white">
             <div className="flex col-span-3">
                 <img
                     onClick={() => toggleMenuHandler()}
@@ -23,8 +58,24 @@ const Head = () => {
                 </a>
             </div>
             <div className="col-span-8 place-content-center">
-                <input className="w-3/5 border border-gray-400 px-3 py-2 rounded-l-full" type="text" placeholder="Search" />
-                <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100">🔍</button>
+                <div>
+                    <input value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    onFocus={() => setShowsuggestion(true)}
+                    onBlur={() => setShowsuggestion(false)}
+                    className="w-3/5 border border-gray-400 px-3 py-2 rounded-l-full" type="text" placeholder="Search" />
+                    <button className="border border-gray-400 p-2 rounded-r-full bg-gray-100">🔍</button>
+                </div>
+
+                {showSuggestion && (<div className="fixed bg-white py-2 px-2 w-[37%] shadow-lg rounded-lg border-gray-100">
+                    <ul>
+                    {
+                        suggestions.map((s) => (
+                            <li key={s} className="py-2 shadow-sm hover:bg-gray-100">🔍 {s}</li>
+                        ))
+                    }
+                    </ul>
+                </div>)}
             </div>
             <div className="col-span-1">
                 <img
@@ -35,4 +86,4 @@ const Head = () => {
     )
 }
 
-export default Head
+export default Head;
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
 
-const GOOGLE_API_KEY = "AIzaSyBr2qCByF46bzCeN8Tt0Wo0U5Y22E1cNQY"
-const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" + GOOGLE_API_KEY
+const GOOGLE_API_KEY = "AIzaSyBrYbROoD2607RflQ2AUk8Kc99nwVng_1Y"
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY
+
+export const SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
