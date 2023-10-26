be18321de4fb2564d410dd2d3fc74004283fe2f2
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-10-26 22:52:24 +0530

    update chat slice

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,4 +2,6 @@
 const GOOGLE_API_KEY = "AIzaSyBrYbROoD2607RflQ2AUk8Kc99nwVng_1Y"
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY
 
-export const SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const LIVE_CHAT_COUNT = 25;
