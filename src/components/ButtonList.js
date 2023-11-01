73f1d709b75e898f1cff647d77c7bfce6d280cdd
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-01 23:04:21 +0530

    update button

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "./Button";
 
-const list = ["All", "Gaming", "Songs", "Live", "Music", "Python", "React", "JavaScript"]
+const list = ["All", "Gaming", "Songs", "Live", "Music", "Python", "React", "JavaScript", "Gaming", "Songs", "Live", "Music", "Chat"]
 
 const ButtonList = () => {
 
     return (
-        <div className="flex">
+        <div className="flex fixed bg-white">
             
             {list.map((name) => 
                 <Button key={name} name={name}/>
