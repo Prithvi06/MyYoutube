92906f8e42fcc22b9a0944d011b8c2736dbf0f33
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-02 23:03:11 +0530

    update sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,34 +13,34 @@ const Sidebar = () => {
         <div className="p-2  w-[5%] fixed bottom-0 top-16">
             <ul className="">
             <Link to={"/"}>
-                <li className="flex flex-col items-center gap-7 px-5  py-6 bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faHouse} className="h-6" />
-                    Home
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faHouse} className="h-5" />
+                    <span className="text-[12px]">Home</span>
                 </li>
             </Link>
-                <li className="flex items-center gap-7 px-5 py-6 hover:bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faWandMagicSparkles} />
-                    Shorts
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faWandMagicSparkles} className="h-5" />
+                    <span className="text-[12px]">Shorts</span>
                 </li>
-                <li className="flex items-center gap-7 px-5 py-6 hover:bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faTicket} />
-                    Subscriptions
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faTicket} className="h-5" />
+                    <span className="text-[10px]">Subscriptions</span>
                 </li>
-                <li className="flex items-center gap-7 px-5 py-6 hover:bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faUserTie} />
-                    Your channel
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faUserTie} className="h-5" />
+                    <span className="text-[12px] text-center">Your channel</span>
                 </li>
-                <li className="flex items-center gap-7 px-5 py-6 hover:bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faClockRotateLeft} />
-                    History
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faClockRotateLeft} className="h-5" />
+                    <span className="text-[12px]">History</span>
                 </li>
-                <li className="flex items-center gap-7 px-5 py-6 hover:bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faCirclePlay} />
-                    Your videos
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faCirclePlay} className="h-5" />
+                    <span className="text-[12px] text-center">Your videos</span>
                 </li>
-                <li className="flex items-center gap-7 px-5 py-6 hover:bg-gray-100 rounded-xl cursor-pointer">
-                    <FontAwesomeIcon icon={faClock} />
-                    Watch later
+                <li className="flex flex-col items-center gap-2 px-8  py-5 hover:bg-gray-200 rounded-xl cursor-pointer">
+                    <FontAwesomeIcon icon={faClock} className="h-5" />
+                    <span className="text-[12px]">Watch later</span>
                 </li>
             </ul>
         </div>
