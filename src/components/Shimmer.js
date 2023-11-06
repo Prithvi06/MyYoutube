37b9f661e7b23925fa0a22a87398e53c7f12a4ac
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-06 23:12:51 +0530

    add shimmer.js

---
+++ src/components/Shimmer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Shimmer = () => {
+    return (
+        <>
+        <div className="flex flex-wrap mt-20">
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100 mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+        </div>
+        </>
+       
+        
+    )
+};
+
+export default Shimmer;
