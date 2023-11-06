746adccf5bd5e9d732b7395a741df46c02b231d1
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-06 23:13:28 +0530

    update helper

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -25,3 +25,36 @@ var nameList = [
 export function generateRandomName() {
     return nameList[Math.floor( Math.random() * nameList.length )];
 };
+
+export function timeSince(date) {
+    var seconds = Math.floor((new Date() - new Date(date)) / 1000);
+    var interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+      return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+
+export const formatCash = n => {
+    if (n < 1e3) return n;
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+    if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+  };
