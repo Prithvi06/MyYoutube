cd528c3f557ad086d9ddec8374a62e59fa9b30a7
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-07 23:01:31 +0530

    add scroll

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,40 +2,55 @@ import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEOS_API } from "../utils/config";
 import VideoCard from "./VideoCard";
 import { Link } from "react-router-dom";
+import Shimmer from "./Shimmer";
 
 const VideoContainer = () => {
     const [videos, setVideos] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         getVideos();
     
-    }, [])
+    }, [isLoading])
 
     const getVideos = async () => {
-        const data = await fetch(YOUTUBE_VIDEOS_API);
-        const json = await data.json();
-        console.log("DATA", json.items)
-        setVideos(json.items)
+        // setIsLoading(true);
+        try {
+            const data = await fetch(YOUTUBE_VIDEOS_API);
+            const json = await data.json();
+            console.log("DATA", json.items)
+            setVideos([...videos, ...json.items])
+        } finally {
+            setIsLoading(false);
+        }
+        
     }
 
-    var infiniteScroll = () => {
-        // End of the document reached?
-        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
-            getVideos();
-            console.log("scrol")
+    const handleScroll = () => {
+        const scrollTop = document.documentElement.scrollTop
+        const scrollHeight = document.documentElement.scrollHeight
+        const clientHeight = document.documentElement.clientHeight
+        if (scrollTop + clientHeight >= scrollHeight) {
+            setIsLoading(true)
         }
-    }
-    window.addEventListener('scroll', infiniteScroll());
-    return (
-        <div className="flex flex-wrap mt-20">
+    };
+      
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [videos]);
+
+    return videos?.length === 0 ? (
+    <Shimmer />) :
+    (
+        <div className="flex absolute -z-20 flex-wrap mt-20">
             {
-                videos.map((video) => (
-                   <Link to={"/watch?v="+video.id} key={video.id}>
-                       <VideoCard key={video.id} info={video} />
+                videos.map((video, index) => (
+                   <Link to={"/watch?v="+video.id} key={index}>
+                       <VideoCard key={index} info={video} />
                    </Link> 
                 ))
             }
-
         </div>
     )
 }
