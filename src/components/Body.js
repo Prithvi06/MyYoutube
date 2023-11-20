8ae98eb532a2dc07d4082f5ec58aaed75be004fe
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-20 23:09:11 +0530

    update body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Head from "./Head";
 import { Outlet } from "react-router-dom";
-
+import ButtonList from "./ButtonList";
 const Body = () => {
     return (
         <div className="flex">
+            <Head />
+            <ButtonList />
             <Sidebar />
             <Outlet />
         </div>
