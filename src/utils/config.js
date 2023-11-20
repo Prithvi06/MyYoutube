4f7f87e58afe6df53fc7a5238ec044c7861bcfa2
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-20 23:09:39 +0530

    update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 
-const GOOGLE_API_KEY = "AIzaSyBDH3L38jVAmVqlM6sTxc4RmXGkKaM5_DU"
+const GOOGLE_API_KEY = "AIzaSyBB9x3MFSBmh7YWvlaaIlfV-kmJhT_FM48"
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY
 
 export const SEARCH_SUGGESTION_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
@@ -9,3 +9,5 @@ export const LIVE_CHAT_COUNT = 25;
 export const CHANNEL_API = "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=channelId&key=" + GOOGLE_API_KEY
 
 export const SEARCH_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=query&key=" + GOOGLE_API_KEY
+
+export const COMMENT_API = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=vId&key=" + GOOGLE_API_KEY
