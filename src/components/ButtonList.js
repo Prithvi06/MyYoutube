9e2db9f0f9a92acece889c38b58cc43945ad5123
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-21 22:41:34 +0530

    add button list

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Button from "./Button";
+import { useSelector } from "react-redux";
 
-const list = ["All", "Gaming", "Songs", "Live", "Music", "Python", "React", "JavaScript", "Gaming", "Songs", "Live", "Music", "Chat"]
+
+const list = ["All", "Gaming", "Songs", "Live", "Music", "Python", "React", "JavaScript", "Gaming2", "Songs2", "Live2", "Music2"]
 
 const ButtonList = () => {
+    const isMenuOpen = useSelector((store) => store.app.isMenuOpen)
+    const isButtonList = useSelector((store) => store.app.isButtonList)
 
-    return (
-        <div className="flex fixed bg-white">
-            
-            {list.map((name) => 
-                <Button key={name} name={name}/>
-            )}
-            
+    return ! isButtonList ? (<></>) :
+    (
+        <div className={`mt-16 ${isMenuOpen ? 'ml-60' : 'ml-24'}`}>
+            <div className="flex fixed -z-10 bg-white">
+                
+                {list.map((name) => 
+                    <Button key={name} name={name}/>
+                )}
+                
+            </div>
         </div>
     );
 };
