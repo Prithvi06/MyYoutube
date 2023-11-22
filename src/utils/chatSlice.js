dac58aedf004b50f97ae51402613ddee22d67415
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-22 23:20:01 +0530

    update slice

--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -4,15 +4,19 @@ import { LIVE_CHAT_COUNT } from "./config";
 const chatSlice = createSlice({
     name: 'chat',
     initialState: {
-        messages: []
+        messages: [],
+        showChat: false
     },
     reducers: {
         addMessage: (state, action) => {
             state.messages.splice(LIVE_CHAT_COUNT, 1)
             state.messages.unshift(action.payload);
         },
+        toggleLiveChat: (state) => {
+            state.showChat = !state.showChat
+        }
     },
 });
 
-export const {addMessage} = chatSlice.actions;
+export const {addMessage, toggleLiveChat} = chatSlice.actions;
 export default chatSlice.reducer;
