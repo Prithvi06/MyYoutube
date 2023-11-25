18ad409db100faa53ac9d1ed325d240f3a2752b1
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-25 22:31:50 +0530

    update shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,22 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-const Shimmer = () => {
+const Shimmer = ({isSearch}) => {
+    console.log("isSearch",isSearch)
+    const isMenuOpen = useSelector((store) => store.app.isMenuOpen)
     return (
-        <>
-        <div className="flex flex-wrap mt-20">
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100 mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
-            <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+        <div className="mt-16">
+            <div className={` ${isSearch === true && isMenuOpen ? 'flex-col ml-60' : isSearch === true && !isMenuOpen ?  'flex-col ml-24' : 'flex'} flex-wrap mt-20`}>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100 mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+                <div className="m-2 w-[25rem] h-[16rem] rounded-lg bg-gray-100  mb-8"></div>
+            </div>
         </div>
-        </>
        
         
     )
