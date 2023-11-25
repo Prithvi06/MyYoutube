9235c8d72c125be52eba8523925780bd6ef1b586
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-11-25 22:32:23 +0530

    update video card

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { formatCash, timeSince } from "../utils/helper";
 import { CHANNEL_API } from "../utils/config";
 
 const VideoCard = ({info}) => {
-    const isMenuOpen = useSelector((store) => store.app.isMenuOpen)
     const {snippet, statistics} = info;
     const {channelId, channelTitle, title, thumbnails, publishedAt} = snippet;
 
@@ -18,18 +16,17 @@ const VideoCard = ({info}) => {
         const data = await fetch(CHANNEL_API.replace("channelId", channelId));
         const json = await data.json();
         setChannelThumnail(json?.items[0]?.snippet?.thumbnails?.default.url)
-        // console.log(json)
     }
 
     return (
-        <div className={`m-2 ${isMenuOpen ? 'w-[25rem]' : 'w-[21rem]' } mb-8`}>
-            <img className="hover:rounded-none rounded-lg w-[400px]" src={thumbnails.medium.url} alt="thumbnails" />
+        <div>
+            <img className="hover:rounded-none rounded-lg w-full" src={thumbnails.medium.url} alt="thumbnails" />
             <div className="flex  pt-2">
             <img src={channelThumnail} className="h-10 rounded-full" alt="channel-img" />
-            <ul className="ml-2">
+            <ul className="ml-2 overflow-x-hidden">
                 <li className="font-bold">{title}</li>
                 <li className="font-semibold text-gray-500">{channelTitle}</li>
-                <li>{formatCash(statistics.viewCount)} views<span className="font-bold">. </span> {timeSince(publishedAt)} Ago</li>
+                <li>{formatCash(statistics?.viewCount)} views<span className="font-bold">. </span> {timeSince(publishedAt)} Ago</li>
             </ul>
             </div>
         </div>
