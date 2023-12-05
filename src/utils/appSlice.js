1311afba9926c818339a48176ee5e46e01132cf7
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-12-05 23:01:20 +0530

    update slice

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -3,17 +3,28 @@ import { createSlice } from "@reduxjs/toolkit"
 const appSlice = createSlice({
     name: "app",
     initialState: {
-        isMenuOpen: true
+        isMenuOpen: true,
+        isButtonList: true,
+        channelId: "",
     },
     reducers: {
         toggleMenu: (state) => {
             state.isMenuOpen = !state.isMenuOpen
         },
+        openMenu: (state) => {
+            state.isMenuOpen = true;
+        },
         closeMenu: (state) => {
             state.isMenuOpen = false;
-        }
+        },
+        openButtonList: (state) => {
+            state.isButtonList = true
+        },
+        closeButtonList: (state) => {
+            state.isButtonList = false;
+        },
     },
 });
 
-export const {toggleMenu, closeMenu} = appSlice.actions
+export const {toggleMenu, openMenu, closeMenu, openButtonList, closeButtonList} = appSlice.actions
 export default appSlice.reducer;
